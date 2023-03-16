6edab3ea78e8e44a4f4e377253a8ffe4dfecefa2
GonzaloMartinezPeralta <126030326+GonzaloMartinezPeralta@users.noreply.github.com>
2023-03-16 01:01:50 -0300

    Initial commit

---
+++ cv-nuevo-java.js
@@ -0,0 +1,22 @@
+function darInfo1() {
+    document.getElementById("texto_experiencia_laboral_más_1").innerHTML = "Jefe de trabajos prácticos de Biología y Biología Animal y Vegetal, materias de segundo y tercer año respectivamente. Encargado del dictado de cursos prácticos y la coordinación de las tareas de los auxiliares en horario de comisión.";
+  }
+
+  function darInfo2() {
+    document.getElementById("texto_experiencia_laboral_más_2").innerHTML = "Auxiliar de Biología y Biología Especial, materias de segundo año. A cargo de las preparaciones de los trabajos prácticos coordinados por el JTP";
+  }
+
+  function darInfo3() {
+    document.getElementById("texto_experiencia_laboral_más_3").innerHTML = "Dictado de cursillos presenciales orientado a ingresantes de las distintas carreras de grado de la facultad, previo al dictado del primer año de cursado";
+  }
+  
+  function darInfo4() {
+    document.getElementById("texto_experiencia_laboral_más_4").innerHTML = "Capacitación en el uso del modelo de espermatozoides de cerdo con énfasis en la fosforilación de proteínas en Laboratorio de Medicina Reproductiva ";
+  }
+  
+  function darInfo5() {
+    document.getElementById("texto_experiencia_laboral_más_5").innerHTML = "Dictado de cursillos presenciales orientado a ingresantes de las distintas carreras de grado de la facultad, previo al dictado del primer año de cursado";
+  }
+
+
+
